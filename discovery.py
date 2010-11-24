be6035fbde73652dbc04620ad986990e4a3fdfcc
Mark Roddy <markroddy@gmail.com>
2010-11-23 22:53:45 -0500

    Importing the contents of unittest2-py3k minus the actual unittest2 implementation which will be pulled from python subversion.

---
+++ discovery.py
@@ -0,0 +1,6 @@
+# This module is now obsolete
+# From Python 2.7 / 3.2 test discovery is built into unittest
+# For Python 2.4-2.6 use the discover module
+# 
+# Download & docs: http://pypi.python.org/pypi/discover
+# Development version: http://code.google.com/p/unittest-ext/source/browse/trunk/discover.py
